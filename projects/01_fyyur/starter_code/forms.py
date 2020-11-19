3253cd16860ef2ce211620abc6e6899dcd16300a
MudatherZaki <57122898+MudatherZaki@users.noreply.github.com>
2020-11-19 20:54:38 +0200

    submission

--- projects/01_fyyur/starter_code/forms.py
+++ projects/01_fyyur/starter_code/forms.py
@@ -2,6 +2,13 @@ from datetime import datetime
 from flask_wtf import Form
 from wtforms import StringField, SelectField, SelectMultipleField, DateTimeField
 from wtforms.validators import DataRequired, AnyOf, URL
+import re
+
+def validate_phone(self, phone):
+    us_phone_regex = '^([0-9]{3})[-][0-9]{3}[-][0-9]{4}$'
+    match = re.search(us_phone_regex, phone.data)
+    if not match:
+        raise ValidationError('Error, phone must be in format xxx-xxx-xxxx')
 
 class ShowForm(Form):
     artist_id = StringField(
@@ -83,7 +90,8 @@ class VenueForm(Form):
         'address', validators=[DataRequired()]
     )
     phone = StringField(
-        'phone'
+        'phone',
+        validators=[DataRequired(), validate_phone]
     )
     image_link = StringField(
         'image_link'
@@ -182,7 +190,8 @@ class ArtistForm(Form):
     )
     phone = StringField(
         # TODO implement validation logic for state
-        'phone'
+        'phone',
+        validators=[DataRequired(), validate_phone]
     )
     image_link = StringField(
         'image_link'
